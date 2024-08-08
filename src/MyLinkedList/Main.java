e30bcbfa7fabd1387464074d92e54b02fee67ab6
Sanjeev Pandey <sanjeevpandey467@gmail.com>
2024-08-08 20:30:57 +0530

    LinkedList

--- src/MyLinkedList/Main.java
+++ src/MyLinkedList/Main.java
@@ -3,11 +3,42 @@ package MyLinkedList;
 public class Main {
     public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList();
+        list.insert(1);
         list.insert(2);
         list.insert(3);
         list.insert(4);
         list.insert(5);
         list.insert(6);
-        System.out.println(list);
+
+//         Sum function
+//        System.out.println(list.sum());
+
+//        To insert a node at beginning of LL
+//        list.insertAtFirst(11);
+//        System.out.println(list);
+
+//        TO insert at particular index
+//        list.insertAt(2, 51);
+//        System.out.println(list);
+
+
+//        To Reverse a LL
+//        list.reverse();
+//        System.out.println(list);
+
+//        Delete node from beginning
+//       list.removeStart();
+
+//       Delete Node from end
+
+//       list.removeEnd();
+//       list.removeEnd();
+//       System.out.println(list);
+
+//        Remove nth node from end
+//        list.removeNthFromEnd(3);
+//        System.out.println(list);
+
     }
+
 }
--- src/MyLinkedList/MyLinkedList.java
+++ src/MyLinkedList/MyLinkedList.java
@@ -19,9 +19,10 @@ public class MyLinkedList {
         tail = null;
     }
 
+    // To insert a node or element
     public void insert(int value){
         Node node = new Node(value);
-        if(head == null){
+        if(isEmpty()){
             head = node;
             tail = node;
         }else{
@@ -30,11 +31,131 @@ public class MyLinkedList {
         }
     }
 
+    // To traverse the LinkedList and print the sum
+
+    public int sum(){
+        Node current = head;
+        int sum = 0;
+        while (current != null){
+            sum += current.data;
+            current = current.next;
+        }
+        return sum;
+    }
+
+    // To insert a node at beginning of LL
+        public void insertAtFirst(int value){
+        Node node = new Node(value);
+        if(isEmpty()){
+           insert(value);
+            return;
+        }
+        node.next = head;
+        head = node;
+        }
+
+        // To insert a node at a particular index
+
+    public void insertAt(int index, int value){
+        if(isEmpty()) throw new IllegalArgumentException();
+        Node node = new Node(value);
+        Node current = head;
+        Node prev = null;
+
+        while (index > 0 && current!=null){
+            prev = current;
+            current = current.next;
+            index--;
+        }
+        prev.next= node;
+        node.next=current;
+
+
+    }
+
+    // TO reverse a LL
+
+    public void reverse(){
+        if(head == null || head.next == null) return;
+        Node prev = null;
+        Node current = head;
+        Node next = head.next;
+
+        while (next != null){
+            current.next = prev;
+            prev = current;
+            current = next;
+            next = next.next;
+        }
+
+        current.next=prev;
+        head = current;
+    }
+
+    // Delete node from beginning
+
+    public void removeStart(){
+        if(isEmpty()) return;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+    }
+
+    // Delete Node from end
+
+    public void removeEnd(){
+        if(isEmpty()) return;
+        if(head == tail){
+            head = tail = null;
+            return;
+        }
+        Node current = head;
+        while (current.next != tail){
+            current = current.next;
+        }
+        current.next = null;
+        tail = current;
+    }
+
+    // Remove nth node from end
+
+    public void removeNthFromEnd(int n){
+        if(isEmpty()) return;
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node fast = head;
+        Node current = head;
+        Node prev = null;
+
+    while(n > 1 && fast != null){
+        fast = fast.next;
+        n--;
+    }
+
+    while(fast.next != null){
+        prev = current;
+        current = current.next;
+        fast = fast.next;
+    }
+    prev.next = current.next;
+    current.next = null;
+    }
+
+
+        // isEmpty function
+
+    public boolean isEmpty(){
+        return head == null && tail==null;
+    }
+
     public String toString(){
         Node current = head;
         StringBuilder result = new StringBuilder();
         while(current!= null){
-            result.append(current.data).append("--->");
+            result.append(current.data).append("-->");
             current = current.next;
         }
         return result.toString();
